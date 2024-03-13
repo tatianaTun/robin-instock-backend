a441767de15b305d65aebb863310f4dafd6dee10
tatianaTun <tatianatun92@gmail.com>
2024-03-13 12:11:33 -0700

    Added api to get list of all warehouses

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5050;
 
+const warehouseRoutes = require('./routes/warehouse-routes');
+
+app.use('/api/warehouses', warehouseRoutes);
 
 app.listen(PORT, () => {
     console.log(`running at http://localhost:${PORT}`);
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,1 +1,9 @@
+const router = require("express").Router();
+const warehouseController = require("../controllers/warehouse-controller");
 
+router
+  .route("/")
+  .get(warehouseController.index)
+
+
+module.exports = router;
