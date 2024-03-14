c4b8f95c7df0e230e2331f791add3a553098795d
linda <lovelittlebear@hotmail.com>
2024-03-14 14:50:26 -0700

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+
 const PORT = process.env.PORT || 5050;
 const inventoriesRoutes = require("./routes/inventories-routes");
 const warehouseRoutes = require("./routes/warehouse-routes");
+
 app.use(express.json());
+app.use(cors());
 // all inventories routes
 app.use("/inventories", inventoriesRoutes);
 // all warehouses routes
