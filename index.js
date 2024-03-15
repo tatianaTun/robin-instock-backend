363c3367d6ac0aa11cff48b28dc0ccdb37c8b9cc
Mary Oje <nwanneka.oje@hotmail.com>
2024-03-15 18:26:34 -0400

    Added /api to each route

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ const inventoriesRoutes = require("./routes/inventories-routes");
 const warehouseRoutes = require("./routes/warehouse-routes");
 
 // all inventories routes
-app.use("/inventories", inventoriesRoutes);
+app.use("/api/inventories", inventoriesRoutes);
+
 
 // all warehouses routes
-app.use("/warehouses", warehouseRoutes);
+app.use("/api/warehouses", warehouseRoutes);
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
 });
